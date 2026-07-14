36d8385d642a6e1ca8e9b0650d483a46364054b6
Lily <lilytarpey.dev@pm.me>
2026-07-15 00:04:07 +0800

    adjust styling on new book page

--- app/(root)/books/new/page.tsx
+++ app/(root)/books/new/page.tsx
@@ -6,10 +6,10 @@ function Page() {
     <main className="container mx-auto">
       <div className="pt-16 grid gap-8">
         <section className="flex flex-col gap-5 text-center items-center">
-          <h1 className="text-4xl md:text-5xl font-serif font-bold text-black tracking-[-0.02em]">
+          <h1 className="text-4xl md:text-5xl font-serif font-bold text-foreground tracking-[-0.02em]">
             Add a New Book
           </h1>
-          <p className="text-md text-black font-serif">
+          <p className="text-md text-foreground font-serif">
             Upload a PDF to generate your interactive interview
           </p>
         </section>
