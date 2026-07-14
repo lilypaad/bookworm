cf6ed12656eabae73374ab7ae8c2102d0e309282
Lily <lilytarpey.dev@pm.me>
2026-07-14 23:28:51 +0800

    change spacing on new book page

--- app/(root)/books/new/page.tsx
+++ app/(root)/books/new/page.tsx
@@ -3,9 +3,9 @@ import UploadForm from "@/components/UploadForm";
 
 function Page() {
   return (
-    <main className="wrapper container">
-      <div className="mx-auto max-w-180 space-y-10">
-        <section className="flex flex-col gap-5">
+    <main className="container mx-auto">
+      <div className="pt-16 grid gap-8">
+        <section className="flex flex-col gap-5 text-center items-center">
           <h1 className="text-4xl md:text-5xl font-serif font-bold text-black tracking-[-0.02em]">
             Add a New Book
           </h1>
