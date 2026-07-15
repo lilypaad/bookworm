f77510617c4f5caff5b352c80485e7d9ee810c33
Lily <lilytarpey.dev@pm.me>
2026-07-15 18:00:15 +0800

    authorise user before attempting blob deletion

--- lib/actions/blob.actions.ts
+++ lib/actions/blob.actions.ts
@@ -1,9 +1,13 @@
 'use server'
 
 import { del } from "@vercel/blob";
+import { auth } from "@clerk/nextjs/server";
 
 export async function deleteBlob(url: string) {
   try {
+    const { userId } = await auth();
+    if(!userId) throw new Error('Unauthorized')
+
     await del(url);
     return { success: true };
   } catch (error) {
