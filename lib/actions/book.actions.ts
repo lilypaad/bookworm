92f347fe64ee674ccd8c5ad58c7dee0e4a32702b
Lily <lilytarpey.dev@pm.me>
2026-07-15 14:16:48 +0800

    add createBook and related server actions

---
+++ lib/actions/book.actions.ts
@@ -0,0 +1,99 @@
+'use server'
+
+import { connectToDatabase } from "@/database/mongoose";
+import { generateSlug, serialiseData } from "@/lib/utils";
+import { CreateBook, TextSegment } from "@/types";
+import Book from "@/database/models/book.model";
+import BookSegment from "@/database/models/book-segment.model";
+
+export async function checkBookExists(title: string) {
+  try {
+    await connectToDatabase()
+
+    const slug = generateSlug(title)
+
+    const existingBook = await Book.findOne({slug}).lean()
+
+    if(existingBook) {
+      return {
+        exists: true,
+        data: serialiseData(existingBook)
+      }
+    }
+  }
+  catch(e) {
+    console.error('Error checking book exists', e)
+    return {
+      exists: false,
+      error: e
+    }
+  }
+}
+
+export async function createBook(data: CreateBook) {
+  try {
+    await connectToDatabase()
+
+    const slug = generateSlug(data.title)
+
+    const existingBook = await Book.findOne({slug}).lean()
+    if(existingBook) {
+      return {
+        success: true,
+        data: serialiseData(existingBook),
+        alreadyExists: true,
+      }
+    }
+
+    // TODO: check subscription limits before creating a book
+
+    const book = await Book.create({ ...data, slug, totalSegments: 0 })
+
+    return {
+      success: true,
+      data: serialiseData(book),
+    }
+  }
+  catch(e) {
+    console.error('Error creating a book', e)
+
+    return {
+      success: false,
+      error: e,
+    }
+  }
+}
+
+export async function saveBookSegments(bookId: string, clerkId: string, segments: TextSegment[]) {
+  try {
+    await connectToDatabase()
+
+    console.log('Saving book segments...')
+
+    const segmentsToInsert = segments.map(({ text, segmentIndex, pageNumber, wordCount }) => ({
+      clerkId,
+      bookId,
+      content: text,
+      segmentIndex,
+      pageNumber,
+      wordCount
+    }))
+
+    await BookSegment.insertMany(segmentsToInsert)
+
+    await Book.findByIdAndUpdate(bookId, { totalSegments: segments.length })
+
+    console.log('Book segments saved successfully.')
+
+    return {
+      success: true,
+      data: { segmentsCreated: segments.length },
+    }
+  }
+  catch(e) {
+    console.error('Error saving book segments', e)
+    await BookSegment.deleteMany({ bookId })
+    await Book.findByIdAndDelete(bookId)
+    console.log('Deleted book segments and book due to failure saving segments.')
+  }
+}
