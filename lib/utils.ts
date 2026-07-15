3efc91ab1b29df4613da76af7625cbe974cd1ceb
Lily <lilytarpey.dev@pm.me>
2026-07-15 14:15:56 +0800

    add slug and data serialisation util funcs

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateSlug(text: string): string {
+  return text
+    .replace(/\.[^/.]+$/, '') // remove file extensions
+    .toLowerCase()
+    .trim()
+    .replace(/[^\ws-]/g, '') // remove special characters
+    .replace(/[\s_]+/g, '-') // replaces spaces/underscores with hyphens
+    .replace(/^-+|-+$/g, '') // remove leading/trailing hyphens
+}
+
+export function serialiseData<T>(data: T): T {
+  return JSON.parse(JSON.stringify(data))
+}
